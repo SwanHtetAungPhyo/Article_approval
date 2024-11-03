721a5628828ffbd49735db3a8b001526603f9c41
SwanHtetAungPhyo <swanhtetaungp@gmail.com>
2024-11-03 17:27:14 +0100

    Project Initialization

---
+++ src/main/java/com/swanhtetaungphyo/article_approval/ArticleApprovalApplication.java
@@ -0,0 +1,13 @@
+package com.swanhtetaungphyo.article_approval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleApprovalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleApprovalApplication.class, args);
+	}
+
+}
